41b54bd9e7ae73a0687094f6a791c780963d3257
pospelov <pospelovlm@yandex.ru>
2018-02-01 09:11:53 +0300

    Опять запороли кодировку

--- Secunda.lua
+++ Secunda.lua
@@ -166,10 +166,10 @@ SetTimer(1000, function()
 end)
 
 ru = function(str)
-	local cyr  = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ"
+	local cyr  = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
 	for i = 1, string.len(cyr) do
 		str = string.gsub(str, stringx.at(cyr, i), "<cyrillic:" .. tostring(i) .. ">")
 	end
 	return str
 end
-if ru "пїЅпїЅпїЅ" ~= "<cyrillic:16><cyrillic:31><cyrillic:15>" then error("test failed - ru()") end
+if ru "абв" ~= "<cyrillic:16><cyrillic:31><cyrillic:15>" then error("test failed - ru()") end
