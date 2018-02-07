36605195b6dda12ef5bd6f4e22474c757cadf0e3
pospelov <pospelovlm@yandex.ru>
2018-02-08 01:37:58 +0300

    Debug.lua, Ru.lua

--- Secunda.lua
+++ Secunda.lua
@@ -16,7 +16,7 @@ dsres = require "data/dsres"
 
 Secunda = {}
 Secunda.sName = "Secunda"
-Secunda.sVersion = "0.5.2"
+Secunda.sVersion = "0.5.3"
 Secunda.sAuthor = "Pospelov"
 
 print ""
@@ -164,21 +164,3 @@ SetTimer(1000, function()
 	print ""
 	print "Secunda is still alive"
 end)
-
-ru = function(str)
-	local cyr  = "éöóêåíãøùçõúôûâàïğîëäæıÿ÷ñìèòüáşÉÖÓÊÅÍÃØÙÇÕÚÔÛÂÀÏĞÎËÄÆİß×ÑÌÈÒÜÁŞ"
-	for i = 1, string.len(cyr) do
-		str = string.gsub(str, stringx.at(cyr, i), "<cy:" .. tostring(i) .. ">")
-	end
-	return str
-end
-if ru "àáâ" ~= "<cy:16><cy:31><cy:15>" then error("test failed - ru()") end
-
-deru = function(str)
-	local cyr  = "éöóêåíãøùçõúôûâàïğîëäæıÿ÷ñìèòüáşÉÖÓÊÅÍÃØÙÇÕÚÔÛÂÀÏĞÎËÄÆİß×ÑÌÈÒÜÁŞ"
-	for i = 1, string.len(cyr) do
-		str = string.gsub(str, "<cy:" .. tostring(i) .. ">", stringx.at(cyr, i))
-	end
-	return str
-end
-if deru "<cy:16><cy:31><cy:15>" ~= "àáâ" then error("test failed - deru()") end
--- SecundaModules/Systems/Debug.lua
+++ SecundaModules/Systems/Debug.lua
@@ -41,128 +41,22 @@ function Debug.OnServerInit()
     return not Debug.IsDeveloper(user)
   end)
 
-  local setexp = Command("/setexp", "sf", "/setexp <skill> <value>", function(user, args)
+  local perk = Command("/perk", "s", "/perk <add/rm>", function(user, args)
     if not Debug.IsDeveloper(user) then
       return true
     end
-    if type(args[1]) == "string" and type(args[2]) == "number" then
-      user:SetSkillExperience(args[1], args[2])
-      user:SendChatMessage(Theme.info .. "Процент опыта для навыка " .. Theme.sel .. args[1]:lower() .. Theme.info .. " теперь равен " .. Theme.sel .. tostring(args[2]));
+    local perk = Perk.LookupByID(0xc07c8)
+    if args[1] == "rm" then
+      user:RemovePerk(perk)
+      user:SendChatMessage(Theme.info .. "Перк отнят")
+      return true
+    elseif args[1] == "add" then
+      user:AddPerk(perk)
+      user:SendChatMessage(Theme.info .. "Перк выдан")
       return true
     end
     return not Debug.IsDeveloper(user)
   end)
-
-  local incrskill = Command("/incrskill", "ss", "/incrskill <userName> <skillName>", function(user, args)
-    if type(args[1]) == "string" and type(args[2]) == "string" then
-      local target = User.Lookup(args[1])
-      if target ~= nil then
-        target:IncrementSkill(args[2])
-        target:Save()
-      end
-      return true
-    end
-    return false
-  end)
-
-  local valhelp = Command("/valhelp", "", Debug.emptyTip, function(user)
-    if not Debug.IsDeveloper(user) then
-      return true
-    end
-    user:SendChatMessage(Theme.info .. "sections: av, avcurrent, skillexp, acc")
-    user:SendChatMessage(Theme.info .. "actions: set, get")
-    user:SendChatMessage(Theme.info .. "Только для значений типа " .. Theme.sel .. "number")
-    return true
-  end)
-
-  local val = Command("/val", "ssssf", "/val <userName> <section> <variableName> <set/get> <value> (/valhelp)", function(user, args)
-    if not Debug.IsDeveloper(user) then
-      return true
-    end
-    local userName = args[1]
-    local section = args[2]
-    local varName = args[3]
-    local action = args[4]
-    local value = args[5]
-    if type(userName) == "string" and type(section) == "string" and type(varName) == "string" and type(action) == "string" and type(value) == "number" then
-      local targets = {}
-
-      -- Form targets
-      if userName == "*" then
-        targets = User.GetAllUsers()
-      else
-        local usr = User.Lookup(userName)
-        if usr then
-          table.insert(targets, usr)
-        else
-          user:SendChatMessage(Theme.error .. "Игрок с именем " .. userName .. " не найден")
-        end
-      end
-
-      if targets ~= nil and #targets ~= 0 then
-        for i = 1, #targets do
-          local target = targets[i]
-          if target ~= nil then
-            local set = nil
-            local get = nil
-            if section == "av" then
-              set = function(newVal)
-                target:SetBaseAV(varName, newVal)
-              end
-              get = function()
-                return target:GetBaseAV(varName)
-              end
-            elseif section == "avcurrent" then
-              set = function(newVal)
-                target:SetCurrentAV(varName, newVal)
-              end
-              get = function()
-                return target:GetCurrentAV(varName)
-              end
-            elseif section == "skillexp" then
-              set = function(newVal)
-                target:SetSkillExperience(varName, newVal)
-              end
-              get = function()
-                return target:GetSkillExperience(varName)
-              end
-            elseif section == "acc" then
-              set = function(newVal)
-                target:SetAccountVar(varName, newVal)
-              end
-              get = function()
-                return target:GetAccountVar(varName)
-              end
-            end
-            if set == nil or get == nil then
-              user:SendChatMessage(Theme.error .. "Секция не найдена (" .. section .. ")")
-              return true
-            end
-            if action == "get" then
-              local res = get()
-              local del = Theme.info .. " -> " .. Theme.sel
-              local eq = Theme.info .. " = " .. Theme.sel
-              user:SendChatMessage(Theme.sel .. target:GetName() .. del .. section .. del .. varName .. eq .. tostring(res))
-            elseif action == "set" then
-              set(value)
-              local res = value
-              local del = Theme.info .. " -> " .. Theme.sel
-              local eq = Theme.info .. " теперь равно " .. Theme.sel
-              user:SendChatMessage(Theme.sel .. target:GetName() .. del .. section .. del .. varName .. eq .. tostring(res))
-              target:Save()
-            else
-              user:SendChatMessage(Theme.error .. "Операция не найдена (" .. action .. ")")
-              return true
-            end
-          end
-        end
-      end
-
-      return true
-    end
-    return false
-  end)
-
 end
 
 return Debug
