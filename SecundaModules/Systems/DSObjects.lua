8c1daabeee58861b3920eb2a09e30b05a46d9b8c
pospelov <pospelovlm@yandex.ru>
2018-02-12 02:51:01 +0300

    fix encoding

--- SecundaModules/Systems/DSObjects.lua
+++ SecundaModules/Systems/DSObjects.lua
@@ -2,7 +2,7 @@ local DSObjects = {}
 
 function DSObjects.OnUserDataSearchResult(user, opcode, res)
   if opcode == "Cont" then
-    user:SendChatMessage(Theme.info .. "Р—Р°РіСЂСѓР¶РµРЅ РѕР±СЉРµРєС‚ РЅРѕРјРµСЂ " .. res:GetID())
+    user:SendChatMessage(Theme.info .. "Загружен объект номер " .. res:GetID())
   end
   return true
 end
