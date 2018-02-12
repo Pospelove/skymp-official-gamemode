01551acbb1d978be5dbb7966d6406db14230a48f
pospelov <pospelovlm@yandex.ru>
2018-02-12 04:10:56 +0300

    hotfix

--- SecundaModules/Systems/DSObjects.lua
+++ SecundaModules/Systems/DSObjects.lua
@@ -3,7 +3,7 @@ local DSObjects = {}
 function DSObjects.OnUserDataSearchResult(user, opcode, rawRes)
   if opcode == "Cont" then
     if WorldObject.Lookup(rawRes:GetID()) == nil then
-      user:SendChatMessage(Theme.info .. "Загружен объект номер " .. res:GetID())
+      user:SendChatMessage(Theme.info .. "Загружен объект номер " .. rawRes:GetID())
       local fileName = tostring(rawRes:GetID())
       local wo = WorldObject.Create(fileName, rawRes)
       wo:Save()
