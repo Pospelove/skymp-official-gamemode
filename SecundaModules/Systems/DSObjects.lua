305a01fb4204eb76802fa3eb96a4e73139ff8f98
pospelov <pospelovlm@yandex.ru>
2018-02-12 05:11:56 +0300

    DataSearch doors

--- SecundaModules/Systems/DSObjects.lua
+++ SecundaModules/Systems/DSObjects.lua
@@ -1,13 +1,17 @@
 local DSObjects = {}
 
 function DSObjects.OnUserDataSearchResult(user, opcode, rawRes)
-  if opcode == "Cont" then
+  if opcode == "Cont" or opcode == "Door" then
     if WorldObject.Lookup(rawRes:GetID()) == nil then
       user:SendChatMessage(Theme.info .. "DS загрузил экземпляр " .. Theme.sel ..  opcode .. "-" .. string.format("%X",rawRes:GetID()))
       local fileName = tostring(rawRes:GetID())
       local wo = WorldObject.Create(fileName, rawRes)
       wo:Save()
     end
+  else
+    pcall(function()
+      rawRes:Delete()
+    end)
   end
   return true
 end
