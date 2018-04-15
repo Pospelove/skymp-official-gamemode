91a9fe7188dc2cad9f3e4b02324e917ea98bda1a
pospelov <pospelovlm@yandex.ru>
2018-04-15 05:12:46 +0300

    hotfix

--- SecundaModules/Systems/ActorValues.lua
+++ SecundaModules/Systems/ActorValues.lua
@@ -9,7 +9,7 @@ function ActorValues.OnUserConnect(user)
 end
 
 function ActorValues.OnPlayerJump(pl)
-    if pl:IsFalling() return true end
+    if pl:IsFalling() then return true end
     local stamina = pl:GetCurrentAV("Stamina")
     pl:SetCurrentAV("Stamina", stamina - 75)
     return true
