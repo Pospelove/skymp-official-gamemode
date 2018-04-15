1385001b28b2655852c806b2bd489db001087079
pospelov <pospelovlm@yandex.ru>
2018-04-15 05:04:37 +0300

    no stamina charge when falling

--- SecundaModules/Systems/ActorValues.lua
+++ SecundaModules/Systems/ActorValues.lua
@@ -9,9 +9,10 @@ function ActorValues.OnUserConnect(user)
 end
 
 function ActorValues.OnPlayerJump(pl)
-  local stamina = pl:GetCurrentAV("Stamina")
-  pl:SetCurrentAV("Stamina", stamina - 75)
-  return true
+    if pl:IsFalling() return true end
+    local stamina = pl:GetCurrentAV("Stamina")
+    pl:SetCurrentAV("Stamina", stamina - 75)
+    return true
 end
 
 return ActorValues
